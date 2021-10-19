51249e1ba92860a9ee71ab9730df7ab4ab3110e2
mahayasa1 <udit.wibawa@gmail.com>
2021-10-19 15:30:36 +0800

    uji coba htaccess

--- app/core/App.php
+++ app/core/App.php
@@ -2,7 +2,13 @@
 class App{
 
 public function __construct(){
-    echo "Web MVC Siap Dijalankan";
-}
+    $url = $this->parseURL();
+    var_dump($url);
+    }
+public function parseURL() {
+    if(isset($_GET["url"])) {
+    $url = $_GET["url"];return $url;
+    }
+    }    
 }
 ?>
