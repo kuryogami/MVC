48fd3d16f888b7a3199d0e67e02ff15354ccf34a
mahayasa1 <udit.wibawa@gmail.com>
2021-10-19 15:33:33 +0800

    lanjutan uji coba

--- app/core/App.php
+++ app/core/App.php
@@ -6,8 +6,11 @@ public function __construct(){
     var_dump($url);
     }
 public function parseURL() {
-    if(isset($_GET["url"])) {
-    $url = $_GET["url"];return $url;
+    if(isset($_GET['url'])) {
+        $url = rtrim($_GET['url'],"/");
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
+        return $url;
     }
     }    
 }
