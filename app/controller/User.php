9543159265f9a37ca96f1ca095df8246f0435e72
mahayasa1 <udit.wibawa@gmail.com>
2021-10-19 16:11:45 +0800

    perbaikan

---
+++ app/controller/User.php
@@ -0,0 +1,10 @@
+<?php
+class User {
+    public function index() {
+      echo “User/index”;
+    }
+    public function profile($nama = "Linux", $pekerjaan = "Devs") {
+      echo "Salam kenal saya $nama, saya seorang $pekerjaan";
+    }
+  }
+?>
